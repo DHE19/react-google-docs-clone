452c4ae28441f4b55e23432e1b6943ac56f19264
dhe19 <info.die@gmail.com>
2022-11-01 00:37:45 -0600

    add toastify and some styles added

--- src/components/EditDocs.tsx
+++ src/components/EditDocs.tsx
@@ -4,23 +4,32 @@ import {useParams} from'react-router-dom'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect, useRef, useState } from 'react';
 import { IDoc } from '../type';
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 interface Props{
     database: Firestore;
 }
 
 const EditDocs:React.FC<Props> = ({database}) => {
+    //used to save the current document opened and use it data to update it
     const [docsDesc, setDocsDesc] = useState<IDoc>({id:'',title:'',body:''});
+    // it used to show de UI to write a basic document 
     const getQuillData = (value:string) => setDocsDesc({...docsDesc,body:value});
+    //reference our docsData's collection
     const collectionRef = collection(database, 'docsData') as CollectionReference<IDoc>
   
     const getData = () =>{
+        //get the reference of the document that we want
         const document = doc(collectionRef, params.id);
-        
-        onSnapshot(document, docs => setDocsDesc(docs.data() ?? {id:'',title:'',body:''}));
+        //set the reference and in the call back return our document, and set on DocsDesc
+        onSnapshot(document, doc => setDocsDesc(doc.data() ?? {id:'',title:'',body:''}));
+
+        console.log(docsDesc);
         
     }
 
+    let params = useParams();
     const isMounted = useRef<boolean>();
     useEffect( () =>{
         if(isMounted.current) return;
@@ -29,9 +38,9 @@ const EditDocs:React.FC<Props> = ({database}) => {
         getData();
     },[]);
     
-    let params = useParams();
     
     useEffect(() =>{
+        //it's called each 1 second
         const updateDocsData = setTimeout (() =>{
             //search the docuemnt by id
             const document = doc(collectionRef,params.id);
@@ -39,20 +48,24 @@ const EditDocs:React.FC<Props> = ({database}) => {
             updateDoc(document,{
                 ...docsDesc
             })
-            .then(() => console.log('Saved'))
-            .catch(() => console.log('Cannot Save'))
+            .then(() => toast.success('Documento Guardado',{autoClose:2000}))
+            .catch(() => toast.error('Erro al guardar el documento',{autoClose: 2000}))
         },1000);
 
         return () => clearTimeout(updateDocsData);
     },[docsDesc])
     return (
-        <div>
+        <div className='editDocs-main'>
+            <ToastContainer/>
             <h1>{docsDesc.title}</h1>
             EditDocs
-            <ReactQuill
-            value={docsDesc.body}
-            onChange={getQuillData}
-            />
+            <div className="editDocs-inner">
+                <ReactQuill
+                className='react-quill'
+                value={docsDesc.body}
+                onChange={getQuillData}
+                />
+            </div>
         </div>
     )
 }
